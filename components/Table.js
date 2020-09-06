e01f2283ee3c7e27220449467536940603499f52
Adith Narayanan <adith85086@gmail.com>
2020-09-06 16:35:53 -0700

    All required functionality added, Food Items populate table

--- components/Table.js
+++ components/Table.js
@@ -1,22 +1,61 @@
-'use strict';
+const { useState, useEffect } = React;
+
+const FoodItem = (name, calories, carbs, quantity, key) => {
+
+    return (
+        <tr key={key}>
+            <th>{name}</th>
+            <th>{calories}</th>
+            <th>{carbs}</th>
+            <th>{quantity}</th>
+        </tr>
+    )
+}
 
 const Table = () => {
 
-    const URL = 'https://founders-takehome-api.herokuapp.com/api/fetch'
+    const [foodItems, setFoodItems] = useState();
 
-    /*
-        TODO:
 
-        Make a GET request to `https://founders-takehome-api.herokuapp.com/api/fetch` (initialized as `URL`)
-        Process response data into an HTML table
+    function formatData(data) {
+        var FoodItemList = [];
+        var foodsList = data["foods"];
+        for (var i = 0; i < foodsList.length; i++) {
+            var foodItem = foodsList[i];
+            FoodItemList.push(FoodItem(foodItem["name"],
+                foodItem["calories"],
+                foodItem["carbohydrates"],
+                foodItem["quantity"],
+                i))
+        }
+        return FoodItemList;
+    }
 
-        Feel free to delegate to as many components as you like or make Table a class-based components.
-    */
+    async function fetchApiData() {
+        const foodApiData = await fetch('https://founders-takehome-api.herokuapp.com/api/fetch')
+        foodApiData
+            .json()
+            .then(foodApiData => setFoodItems(formatData(foodApiData)))
+    }
+
+    useEffect(() => {
+        fetchApiData();
+    }, []);
 
     return (
         <section>
             <table>
-                {/* Table Contents */}
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Calories</th>
+                        <th>Carbs</th>
+                        <th>Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {foodItems}
+                </tbody>
             </table>
         </section>
     )
